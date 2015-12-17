b26b2d94aece3b8fc4c46933ecbc39ee1f11c989
Cristian Poley <cripolgon@gmail.com>
2015-12-17 17:33:37 +0100

    Borrado PruebaTest.php y terminado testVote

--- tests/UnitarioTest.php
+++ tests/UnitarioTest.php
@@ -9,9 +9,25 @@ class UnitarioTest extends PHPUnit_Framework_TestCase{
 		$url = 'http://localhost/egc/src/get_votes.php?votation_id=' . $id_vote;
 		$string = file_get_contents($url);
 		fwrite(STDERR, print_r($string,TRUE));
+		
 	}
+	
 	// Vote
-	public function testVote(){
+	public function testVote(){	
+		$jsondata['vote'] = 'VotoPruebaTest';
+		$jsondata['votation_id'] = "3";
+		$url = 'http://localhost/egc/src/vote.php';
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsondata));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		$response = curl_exec($ch);
+		curl_close($ch);
+		echo $response;
+		
+		$this->assertEquals($response,'{"msg":"1"}');
 		
 	}	
 }
