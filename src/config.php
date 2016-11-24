6a3a95b0cce8dc3969738c8b393ca2191aaa0dee
Antonio Arciniega <antarccub@gmail.com>
2016-11-24 21:36:38 +0100

    Instancia inicial de Laravel

--- src/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "egc1516";
-$dbname = "storage";
-
-?>
--- src/services/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-session_start();
-//Esto le da un tiempo de vida a la variable token de la cookie nulo. Por lo que basicamente elimina la variable.
-setcookie("token",'',1, '/');
-header("Location:../index.php");
-die();
-?>
