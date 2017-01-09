08d994b152dde3ca4c9688d862c6fd75857c4d15
Antonio Arciniega <antarccub@gmail.com>
2017-01-09 19:29:34 +0100

    Fix on return statement

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -10,10 +10,10 @@ class Option extends Model
     protected $fillable = ['name', 'survey_id'];
 
     public function votes(){
-        $this->hasMany('App\Models\Vote', 'id');
+        return $this->hasMany('App\Models\Vote', 'id');
     }
 
     public function survey(){
-        $this->belongsTo('App\Models\Survey', 'id');
+        return $this->belongsTo('App\Models\Survey', 'id');
     }
 }
