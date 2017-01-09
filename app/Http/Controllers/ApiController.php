363dcd0ae92a34a5c2172957c852f7145952d134
Antonio Arciniega <antarccub@gmail.com>
2017-01-09 17:13:55 +0100

    Added Swagger config files

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antarccub
+ * Date: 09/01/2017
+ * Time: 17:08
+ */
+
+namespace app\Http\Controllers;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+/**
+ * Class ApiController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="",
+ *     host="laravel.localhost",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="Sample API",
+ *         @SWG\Contact(name="", url=""),
+ *     ),
+ *     @SWG\Definition(
+ *         definition="Error",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *             format="int32"
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
+
+class ApiController extends Controller
+{
+
+}
